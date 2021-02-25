79612dc860de1e9bf4ca4a0b1b3a691ef0fc464a
leandrocodes <leandrovianacodes@gmail.com>
2021-02-25 16:29:21 -0400

    improving readability at list rendering

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -5,16 +5,21 @@ import Style from '../style'
 import products from './products'
 
 export default (props) => {
-  return (
-    <>
-      <Text style={Style.text}>Lista de Produtos</Text>
-      {products.map((product) => {
+
+  function list() {
+     return products.map (product => {
         return (
-          <Text>
+          <Text key={product.id}>
             ({product.id}) - {product.name} - R$: {product.price.toFixed(2)}
           </Text>
         )
-      })}
+     }) 
+  }
+
+  return (
+    <>
+      <Text style={Style.text}>Lista de Produtos</Text>
+      {list()}
     </>
   )
 }
